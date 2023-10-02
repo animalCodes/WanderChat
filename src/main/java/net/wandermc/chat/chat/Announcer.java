467dd6491a42121cd6a8a440cc38900ace19a3fd
animalCodes <84966313+animalCodes@users.noreply.github.com>
2023-10-02 18:42:19 +0100

    Don't announce tips if server is empty
    Stop TipScheduler if no players are online, restart on player join.

--- src/main/java/net/wandermc/chat/chat/Announcer.java
+++ src/main/java/net/wandermc/chat/chat/Announcer.java
@@ -16,12 +16,15 @@
  */
 package net.wandermc.chat.chat;
 
-import java.util.Stack;
 import java.util.EmptyStackException;
+import java.util.List;
+import java.util.Stack;
 
 import net.kyori.adventure.text.format.Style;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
+import net.kyori.adventure.text.format.TextDecoration;
 
 import org.bukkit.Server;
 import org.bukkit.scheduler.BukkitRunnable;
@@ -32,16 +35,30 @@ public class Announcer {
             .color(NamedTextColor.WHITE)
             .decorate().build();
 
+    private TipScheduler tipScheduler;
     private Stack<AnnouncementTask> tasks;
 
     private Server server;
     private JavaPlugin plugin;
+
+    private List<String> tipsList;
+    private int tipsDelay = 0;
 
     public Announcer(Server server, JavaPlugin plugin) {
         this.server = server;
         this.plugin = plugin;
         this.tasks = new Stack<AnnouncementTask>();
     }
+
+    /**
+     * Logs `message` to console.
+     *
+     * @param message The message to send.
+     */
+    public void log(String message) {
+        // TODO clean this up
+        this.plugin.getLogger().info(message);
+    }    
 
     /**
      * Announces `message` prefixed with `prefix` to all online players
@@ -87,6 +104,7 @@ public class Announcer {
         return task;
     }
 
+    // For announcing `prefix` and `message` after a set period
     public class AnnouncementTask extends BukkitRunnable {
         private Component prefix;
         private Component message;
@@ -107,6 +125,75 @@ public class Announcer {
 
         public Component getMessage() {
             return this.message;
+        }
+    }
+
+    /**
+     * Starts a TipScheduler.
+     *
+     * Can also be used to update the tips list and delay used for future TipSchedulers.
+     * 
+     * @param tips The list of tips to choose from.
+     * @param delay The delay in minutes to pause between each tip announcement.
+     */
+    public void startTipScheduler(List<String> tips, int delay) {
+        this.tipsList = tips;
+        this.tipsDelay = delay;
+
+        this.startTipScheduler();
+    }
+
+    /**
+     * Attempts to start a TipScheduler, using the tip list and delay previously given to this Announcer.
+     *
+     * Announcer.startTipScheduler(tips, delay) MUST be run before this.
+     *
+     * @return Whether a TipScheduler was successfully started.
+     */
+    public boolean startTipScheduler() {
+        if (this.tipsDelay != 0 && this.tipsList != null) {
+            this.log("Starting tip schedule");
+            this.tipScheduler = new TipScheduler(this.tipsList);
+            // TODO set back to five minute initial delay
+            this.tipScheduler.runTaskTimer(this.plugin, 20*60*1, this.tipsDelay);
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Gets the current TipScheduler
+     *
+     * @return Current TipScheduler
+     */
+    public TipScheduler getTipScheduler() {
+        return this.tipScheduler;
+    }
+
+    // For sending a random tip from `tips` every few minutes
+    public class TipScheduler extends BukkitRunnable {
+        private List<String> tips;
+
+        private TipScheduler(List<String> tips) {
+            this.tips = tips;
+        }
+
+        public void run() {
+            if (server.getOnlinePlayers().isEmpty()) {
+                plugin.getLogger().info("Server empty, stopping tip schedule");
+                this.cancel();
+                return;
+            }
+            announce(Component.text("[Tip] ").decorate(TextDecoration.BOLD), getRandomTip());
+        }
+
+        /**
+         * Gets a random tip from `this.tips` as a TextComponent
+         *
+         * @return A random tip
+         */
+        private TextComponent getRandomTip() {
+            return Component.text(this.tips.get((int) (Math.random() * this.tips.size())));
         }
     }
 }
